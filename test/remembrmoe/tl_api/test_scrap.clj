1756cd86320ef8803d83e41f07cab285f71b4240
dgellow <samuel.elborai@gmail.com>
2016-05-23 12:52:54 +0700

    randomly select a line, direction and station before running crawler tests

--- test/remembrmoe/tl_api/test_scrap.clj
+++ test/remembrmoe/tl_api/test_scrap.clj
@@ -3,6 +3,21 @@
             [remembrmoe.tl-api.scrap :as scrapper]
             [net.cgrand.enlive-html :as html]))
 
+(defn select-item [coll]
+  (rand-nth coll))
+
+(def line (select-item (scrapper/fetch-lines)))
+(deftest selected-line
+  (is (not (nil? line))))
+
+(def direction (select-item (scrapper/fetch-directions line)))
+(deftest selected-direction
+  (is (not (nil? direction))))
+
+(def station (select-item (scrapper/fetch-stations direction)))
+(deftest selected-station
+  (is (not (nil? station))))
+
 (deftest fetch-tl-live-root-url
   (let [page (scrapper/fetch-url
               "http://www.t-l.ch/tl-live-mobile/index.php")]
@@ -20,60 +35,36 @@
     (is (every? :name lines))))
 
 (deftest fetch-directions-page
-  (let [line (first (scrapper/fetch-lines))
-        page (scrapper/fetch-url (scrapper/make-direction-url line))]
-    (is (not (nil? line)))
+  (let [page (scrapper/fetch-url (scrapper/make-direction-url line))]
     (is (not (empty? page)))
     (is (not (empty? (html/select page scrapper/selector-directions))))))
 
 (deftest fetch-directions
-  (let [line (first (scrapper/fetch-lines))
-        directions (scrapper/fetch-directions line)]
-    (is (not (nil? line)))
+  (let [directions (scrapper/fetch-directions line)]
     (is (not (empty? directions)))
     (is (every? :id directions))
     (is (every? :direction directions))))
 
 (deftest fetch-stations-page
-  (let [line (first (scrapper/fetch-lines))
-        direction (first (scrapper/fetch-directions line))
-        page (scrapper/fetch-url (scrapper/make-station-url direction))]
-    (is (not (nil? line)))
-    (is (not (nil? direction)))
+  (let [page (scrapper/fetch-url (scrapper/make-station-url direction))]
     (is (not (empty? page)))
     (is (not (empty? (html/select page scrapper/selector-stations))))))
 
 (deftest fetch-stations
-  (let [line (first (scrapper/fetch-lines))
-        direction (first (scrapper/fetch-directions line))
-        stations (scrapper/fetch-stations direction)]
-    (is (not (nil? line)))
-    (is (not (nil? direction)))
+  (let [stations (scrapper/fetch-stations direction)]
     (is (not (empty? stations)))
     (is (every? :id stations))
     (is (every? :id-stop stations))))
 
 (deftest fetch-horaire-page
-  (let [line (first (scrapper/fetch-lines))
-        direction (first (scrapper/fetch-directions line))
-        station (first (scrapper/fetch-stations direction))
-        page (scrapper/fetch-url
+  (let [page (scrapper/fetch-url
               (scrapper/make-horaire-url
                (:id line) (:id direction) (:id station)))]
-    (is (not (nil? line)))
-    (is (not (nil? direction)))
-    (is (not (nil? station)))
     (is (not (empty? page)))
     (is (not (empty? (html/select page scrapper/selector-horaire))))))
 
 (deftest fetch-horaire
-  (let [line (first (scrapper/fetch-lines))
-        direction (first (scrapper/fetch-directions line))
-        station (first (scrapper/fetch-stations direction))
-        horaire (scrapper/fetch-horaire
+  (let [horaire (scrapper/fetch-horaire
                  (:id line) (:id direction) (:id station))]
-    (is (not (nil? line)))
-    (is (not (nil? direction)))
-    (is (not (nil? station)))
     (is (not (empty? horaire)))
     (is (every? #(not (clojure.string/blank? %)) horaire))))
