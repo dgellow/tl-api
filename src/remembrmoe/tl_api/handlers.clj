00bb4310330f339ca4de20ff20665625d570d11e
dgellow <samuel.elborai@gmail.com>
2016-05-31 12:58:32 +0700

    remove things we don't need in handlers.clj

--- src/remembrmoe/tl_api/handlers.clj
+++ src/remembrmoe/tl_api/handlers.clj
@@ -1,14 +1,9 @@
 (ns remembrmoe.tl-api.handlers
-  (:require [ring.util.response :as response]
-            [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
+  (:require [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
             [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response :as r]
             [compojure.core :refer [defroutes GET context]]
             [taoensso.timbre :as timbre]
             [dgellow.utils.web-api :refer [success method-not-found]]
-            [remembrmoe.tl-api.state :as state]
-            [remembrmoe.tl-api.scrap :as scrapper]
-            [remembrmoe.tl-api.query :as q]
             [remembrmoe.tl-api.api-v1 :as v1]))
 
 (defroutes app-routes
