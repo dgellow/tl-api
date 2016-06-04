0b1b8b42226217ea9dddc9181f2788c81cd6215c
dgellow <samuel.elborai@gmail.com>
2016-06-05 04:31:03 +0700

    update get-diections to return :from and :to values

--- src/remembrmoe/tl_api/query.clj
+++ src/remembrmoe/tl_api/query.clj
@@ -29,7 +29,7 @@
   (let [line (line-by-name-or-id tl-state line-name-or-id)]
     {:id-line (:id line)
      :name-line (:name line)
-     :directions (map #(select-keys % [:id :direction])
+     :directions (map #(select-keys % [:id :direction :from :to])
                    (:directions line))}))
 
 (defn get-stations [tl-state line-name-or-id direction-id]
